b668f1cf626e2dd345c5a011f35e8e33526920b9
Antoine CARON <acaron@scaleway.com>
2025-11-01 23:30:10 +0100

    fix: fix tf fmt'

--- modules/scw-registry/main.tf
+++ modules/scw-registry/main.tf
@@ -17,8 +17,8 @@ resource "scaleway_registry_namespace" "main" {
 resource "scaleway_iam_policy" "registry_push" {
   count = var.create_push_policy ? 1 : 0
 
-  name        = "${var.registry_name}-push-policy"
-  description = "Policy to allow push to ${var.registry_name} registry"
+  name           = "${var.registry_name}-push-policy"
+  description    = "Policy to allow push to ${var.registry_name} registry"
   application_id = var.application_id
 
   rule {
